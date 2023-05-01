56ec4beb4fb7893136d6e1eba723e718fa32ed2f
AbdullahAlMomen <momenshovon21@gmail.com>
2023-05-01 21:45:32 +0600

    Poject Create

---
+++ DotNetCore_MVC_CRUD_with_EFCore/DotNetCore_MVC_CRUD_with_EFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCore_MVC_CRUD_with_EFCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
